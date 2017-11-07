ac4d5f7e31cc71914278406ebb053ca407f8aafa
caishengjian <32316381+caishengjian@users.noreply.github.com>
2017-11-07 09:14:50 +0800

    搜索编写

---
+++ Clothing.Repository/SearchHistoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clothing.IRepository;//
+using Clothing.Model;//
+
+namespace Clothing.Repository
+{
+   public class SearchHistoryRepository:BaseRepository<SearchHistory>,ISearchHistoryRepository
+    {
+    }
+}
