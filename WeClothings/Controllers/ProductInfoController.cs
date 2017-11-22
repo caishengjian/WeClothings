bf860bf88499838988e32a7866302daa61f7ae7a
caishengjian <32316381+caishengjian@users.noreply.github.com>
2017-11-22 09:12:22 +0800

    页面加载即将完成

---
+++ WeClothings/Controllers/ProductInfoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeClothings.Controllers
+{
+    public class ProductInfoController : Controller
+    {
+        // GET: ProductInfo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Product_xq()
+        {
+            return View();
+        }
+    }
+}
